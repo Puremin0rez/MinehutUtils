3b6653566a83e9cf93bdde5af17c80e1096caca9
Dylan Xaldin <Puremin0rez515@gmail.com>
2026-09-24 23:45:37 -0500

    perf: run /status and /network requests at the same time

--- src/main/kotlin/me/santio/minehututils/commands/impl/NetworkCommand.kt
+++ src/main/kotlin/me/santio/minehututils/commands/impl/NetworkCommand.kt
@@ -2,6 +2,8 @@ package me.santio.minehututils.commands.impl
 
 import com.google.auto.service.AutoService
 import dev.minn.jda.ktx.interactions.commands.Command
+import kotlinx.coroutines.async
+import kotlinx.coroutines.supervisorScope
 import me.santio.minehututils.commands.SlashCommand
 import me.santio.minehututils.coroutines.await
 import me.santio.minehututils.ext.formatted
@@ -47,32 +49,37 @@ class NetworkCommand : SlashCommand {
         // The Minehut API can take longer than Discord's 3 second window to respond
         event.deferReply().await()
 
-        val playerDist = request(event) { Minehut.players() } ?: run {
-            fail(event, EmbedFactory.error("Failed to fetch player statistics", event.guild))
-            return
-        }
+        supervisorScope {
+            val network = async { Minehut.network() }
 
-        val status = request(event) { Minehut.network() } ?: run {
-            fail(event, EmbedFactory.error("Failed to fetch network statistics", event.guild))
-            return
-        }
+            val playerDist = request(event) { Minehut.players() } ?: run {
+                network.cancel()
+                fail(event, EmbedFactory.error("Failed to fetch player statistics", event.guild))
+                return@supervisorScope
+            }
 
-        event.hook.editOriginalEmbeds(
-            EmbedFactory.default(
-                """
-            | :bar_chart: **Network Stats**
-            | 
-            | **Servers**: ${status.serverCount}/${status.serverMax}
-            | **Ram**: ${ceil((status.ramCount ?: 0) / 1000.0).toInt()}GB
-            |
-            | **Players**: ${status.playerCount?.formatted()}
-            | → Java: ${playerDist.javaTotal?.formatted()} (Lobby: ${playerDist.javaLobby?.formatted()}, Servers: ${playerDist.javaPlayerServer?.formatted()})
-            | → Bedrock: ${playerDist.bedrockTotal?.formatted()} (Lobby: ${playerDist.bedrockLobby?.formatted()}, Servers: ${playerDist.bedrockPlayerServer?.formatted()})
-            |
-            | *View player statistics at [Minehut Track](https://track.gamersafer.systems/)*
-            """.trimMargin()
-            ).build()
-        ).queue()
+            val status = request(event) { network.await() } ?: run {
+                fail(event, EmbedFactory.error("Failed to fetch network statistics", event.guild))
+                return@supervisorScope
+            }
+
+            event.hook.editOriginalEmbeds(
+                EmbedFactory.default(
+                    """
+                | :bar_chart: **Network Stats**
+                | 
+                | **Servers**: ${status.serverCount}/${status.serverMax}
+                | **Ram**: ${ceil((status.ramCount ?: 0) / 1000.0).toInt()}GB
+                |
+                | **Players**: ${status.playerCount?.formatted()}
+                | → Java: ${playerDist.javaTotal?.formatted()} (Lobby: ${playerDist.javaLobby?.formatted()}, Servers: ${playerDist.javaPlayerServer?.formatted()})
+                | → Bedrock: ${playerDist.bedrockTotal?.formatted()} (Lobby: ${playerDist.bedrockLobby?.formatted()}, Servers: ${playerDist.bedrockPlayerServer?.formatted()})
+                |
+                | *View player statistics at [Minehut Track](https://track.gamersafer.systems/)*
+                """.trimMargin()
+                ).build()
+            ).queue()
+        }
     }
 
 }
