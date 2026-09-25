a298e9c4477f235ef6d928c238da499f8526c6a3
Dylan Xaldin <Puremin0rez515@gmail.com>
2026-09-24 23:45:37 -0500

    perf: look up marketplace listings by message id

--- src/main/kotlin/me/santio/minehututils/marketplace/MarketplaceListener.kt
+++ src/main/kotlin/me/santio/minehututils/marketplace/MarketplaceListener.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.launch
 import me.santio.minehututils.bot
 import me.santio.minehututils.cooldown.Cooldown
 import me.santio.minehututils.cooldown.CooldownManager
+import me.santio.minehututils.coroutines.await
 import me.santio.minehututils.coroutines.exceptionHandler
 import me.santio.minehututils.database.DatabaseHandler
 import me.santio.minehututils.factories.EmbedFactory
@@ -75,7 +76,7 @@ object MarketplaceListener : ListenerAdapter() {
         val postedBy = message.postedBy
         val type = message.type
         scope.launch(exceptionHandler) {
-            val postedByUser = bot.retrieveUserById(postedBy).complete()
+            val postedByUser = bot.retrieveUserById(postedBy).await()
             val log = GuildLogger.of(channel.guild).log(
                 """
                     :identification_card: User: ${postedByUser?.asMention} *(${postedByUser?.name} - ${postedByUser?.id})*
